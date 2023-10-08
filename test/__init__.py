baa1548beda8f727f407adb37bf50db2df1ef8a5
Bart Thate <bthate@dds.nl>
2023-10-08 19:06:33 +0200

    add init

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,10 @@
 # This file is placed in the Public Domain.
+#
+#
 
 
-"test imports"
+import os
+import sys
 
 
-import os, sys
-
-
-NAME = "zelf"
-sys.path.insert(0, os.path.join(os.getcwd(), "..", NAME))
-print(sys.path)
+sys.path.insert(0, os.getcwd())

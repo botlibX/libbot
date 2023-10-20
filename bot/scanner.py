0b80213004716503a6ab976e4f75568c9f1c8b08
botlib <botlib@proton.me>
2023-10-20 14:51:53 +0200

    add parsers/scanner

---
+++ bot/scanner.py
@@ -0,0 +1,39 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0115,C0116,E0402,W0718,W0212
+
+
+"scanning"
+
+
+from .handler import Handler
+from .storage import Storage
+from .threads import launch
+from .utility import spl
+
+
+def __dir__():
+    return (
+            'scan',
+           )
+
+
+def scan(pkg, modnames="", initer=False) -> []:
+    if not pkg:
+        return []
+    scanned = []
+    threads = []
+    for modname in spl(modnames):
+        module = getattr(pkg, modname, None)
+        if not module:
+            continue
+        scanned.append(modname)
+        Handler.scan(module)
+        Storage.scan(module)
+        if initer:
+            try:
+                module.init
+            except AttributeError:
+                continue
+            threads.append(launch(module.init, name=f"init {modname}"))
+    return threads

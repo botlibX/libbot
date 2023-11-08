3fde05d1f5aaeedeba7ea838cac3925283c1805a
libbot <libbotx@gmail.com>
2023-11-08 14:37:32 +0100

    add test

---
+++ test/test_none.py
@@ -0,0 +1,15 @@
+# This file is placed in the Public Domain.
+#
+#
+
+
+"none"
+
+
+import unittest
+
+
+class TestNone(unittest.TestCase):
+
+    def test_none(self):
+        self.assertTrue(not None)
